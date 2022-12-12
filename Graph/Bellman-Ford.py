d6cc681fd5ba180db2b6ebcd2ff257fbb1dce5d2
Sunayoru <120330360+Sunayoru@users.noreply.github.com>
2022-12-12 15:18:18 +0800

    Update Bellman-Ford.py

--- Graph/Bellman-Ford.py
+++ Graph/Bellman-Ford.py
@@ -1,1 +1,9 @@
-
+distance = [float('inf') for _ in range(n + 1)]
+for k in range(1, n):
+    for u in range(1, n + 1):
+        for v, w in adj[u]:
+            distance[v] = min(distance[v], distance[u] + w)
+for u in range(1, n + 1):
+    for v, w in adj[u]:
+        if distance[v] > distance[u] + w:
+            report_negative_cycle()
