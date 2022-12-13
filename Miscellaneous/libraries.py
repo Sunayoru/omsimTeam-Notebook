ea02c1ac31f0f3bb8f9af765cbc9b9c25f48d63b
Rafael Sheen Acuna <120330360+Sunayoru@users.noreply.github.com>
2022-12-13 21:32:45 +0800

    Create libraries.py

---
+++ Miscellaneous/libraries.py
@@ -0,0 +1,19 @@
+#itertools
+
+from itertools
+accumulate() #for prefix sums
+product() #cartesian product
+permutations(); p, r #r-length tuples
+combinations() p, r #r-length tuples, no repeated elements
+combinations_with_replacement() p, r #r-length tuples with repeated elements
+
+from collections import Counter
+counter = Counter(arr)
+top_three = counter.most_common(3)
+
+from math
+acos(); asin(); atan() #for inverse trig
+cos(); sin(); tan() #for trig
+facotiral()
+log()
+log10()
