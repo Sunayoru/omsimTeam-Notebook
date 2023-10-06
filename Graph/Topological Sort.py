b5a8927ed4ce0f592d77bc2e9b703f85ce0bd3f7
Rafael Sheen Acuna <120330360+Sunayoru@users.noreply.github.com>
2023-10-06 11:28:26 +0800

    Update Topological Sort.py

--- Graph/Topological Sort.py
+++ Graph/Topological Sort.py
@@ -1,18 +1,37 @@
-visited = [False for _ in range(n + 1)]
-in_dfs_stack = [False for _ in range(n + 1)]
-topologically_sorted = []
-
-def toposort(u):
-    in_dfs_stack[u] = True
-    visited[u] = True
-    for v in rev_adj[u]:
-        if in_dfs_stack[v]:
-            report_cycle()
-        elif not visited[v]:
-            toposort(v)
-    topologically_sorted.append(u)
-    in_dfs_stack[u] = False
-
-for u in range(1, n + 1):
-    if not visited[u]:
-        toposort(u)
+import heapq as hq
+def topologicalSort():
+    V = len(adj)
+    in_degree = [0] * V
+    for u in range(V):
+        for x in adj[u]:
+            in_degree[x] += 1
+    s = []
+    for i in range(V):
+        if in_degree[i] == 0:
+            hq.heappush(s, -i)
+    cnt = 0
+    top_order = []
+    while s:
+        u = -hq.heappop(s)
+        top_order.append(u)
+        for x in adj[u]:
+            in_degree[x] -= 1
+            if in_degree[x] == 0:
+                hq.heappush(s, -x)
+        cnt += 1
+    if cnt != V:
+        print("IMPOSSIBLE")
+        return
+    ans = []
+    for i in range(len(top_order)):
+        ans.append(top_order[i])
+    ans.pop()
+    print(*ans)
+for x in range(int(input())):
+    done = False
+    n, m = [int(x) for x in input().split()]
+    adj = [[] for _ in range(n + 1)]
+    for _ in range(m):
+        u, v = [int(x) for x in input().split()]
+        adj[u].append(v)
+    topologicalSort()
